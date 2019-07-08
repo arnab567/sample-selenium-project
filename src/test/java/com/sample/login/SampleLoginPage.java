3a2e383bed32ae430b8daa7002ed052147ca3852
Arnab Mandal <arnab.mandal@kp.org>
2019-07-08 20:56:42 +0530

    renamed the generated file

--- src/test/java/com/sample/login/SampleLoginPage.java
+++ src/test/java/com/sample/login/SampleLoginPage.java
@@ -28,7 +28,8 @@ public class SampleLoginPage {
 		driver.findElement(By.xpath("//input[@value='Log In']")).isEnabled();
 
 		File scr = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		File save = new File("../Images/actual/" + driver.getTitle() + "_actual.png");
+		System.out.println(driver.getTitle().replaceAll(" ", "").replaceAll("-", "_"));
+		File save = new File("../Images/actual/" + driver.getTitle().replaceAll(" ", "").replaceAll("–", "_") + "_actual.png");
 		try {
 			FileUtils.copyFile(scr, save);
 		} catch (IOException e) {
